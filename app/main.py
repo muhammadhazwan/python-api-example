0d22d442e233a4ba9a4b6af5e9998c3a7ac83c13
muhammadhazwan <122425114+muhammadhazwan@users.noreply.github.com>
2023-11-14 09:21:57 +0000

    Initial Commit

---
+++ app/main.py
@@ -0,0 +1,17 @@
+
+from .schemas import Item
+from fastapi import FastAPI, Query, Path
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, query_param: str = Query(None, title="Query Parameter Example")):
+    return {"item_id": item_id, "query_param": query_param}
+
+@app.post("/items/")
+def create_item(item: Item):
+    return item.dict()
